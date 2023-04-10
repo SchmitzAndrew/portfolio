c944a32e2075073a176e895a417317b2413da62a
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-10 02:35:04 -0700

    add name gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className="bg-black min-h-screen">
-        <h1 className= "underline text-white">
-            Hello World! :)
-        </h1>
-    </main>
-  )
+    return (
+        <main className="bg-black min-h-screen">
+            <h1 className="title-gradient text-8xl font-bold" >
+                Andrew Schmitz
+            </h1>
+        </main>
+    )
 }
