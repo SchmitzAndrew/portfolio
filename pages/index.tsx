1ce2ddbe2d19fe648d541d39c957568c2bda3796
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-11 02:45:06 -0700

    center header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
         <main className="bg-black min-h-screen">
-            <h1 className="title-gradient text-8xl font-bold" >
-                Andrew Schmitz
-            </h1>
+            <div className = "flex items-center justify-center">
+                <h1 className="title-gradient text-8xl font-bold" >
+                    Andrew Schmitz
+                </h1>
+            </div>
         </main>
     )
 }
