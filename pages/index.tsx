d8441003aad59e96d13c6a570fbfff51564b58d6
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-04-12 03:19:32 -0700

    center header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main className="bg-black min-h-screen">
-            <div className = "flex items-center justify-center">
-                <h1 className="title-gradient text-8xl font-bold" >
+        <div className='bg-black min-h-screen'>
+            <div className="flex justify-center">
+                <h1 className="title-gradient text-6xl md:text-8xl font-bold text-center">
                     Andrew Schmitz
                 </h1>
             </div>
-        </main>
-    )
+            <div className="max-w-7xl mx-auto">
+                <p className="text-white">I&apos;m a software developer.</p>
+            </div>
+        </div>
+    );
 }
+
+
