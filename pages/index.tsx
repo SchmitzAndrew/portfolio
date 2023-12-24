ea6d5c0a4a69e4ffdf07c5e58eb9ac02baed3e28
Andrew Schmitz <schmitzandrew03@gmail.com>
2023-12-24 11:53:08 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,13 +31,15 @@ export default function Index({posts}: {posts: PostMeta[]}) {
                 <div className="max-w-4xl mx-auto pt-10 pb-6 px-6">
                     <div className="text-slate-300 font-semibold font-Rubik">
                         <p className="md:text-lg">I&apos;m a software developer creating &nbsp;
-                            <a href="https://usestylize.com" target="_blank" className="underline text-sky-400">usestylize.com</a>.
+                            <a href="https://usestylize.com" target="_blank" className="underline text-sky-400">usestylize.com</a> &nbsp;
+                            and 
+                            <a href="https://usemoment.ai" target="_blank" className="underline text-sky-400">usemoment.ai</a>.
                             I'm also studying Data Science at UCSD, but honestly, I hate it. I'm terrified of the idea
                             of growing up and becoming a spreadsheet monkey or developing algorithms just to get
                             people to click on ads more.
                         </p>
                         <p className="pt-4 text-lg">
-                            Instead, I&apos;m trying to do my own thing. I'm not really sure what I want to do with my life yet, but it's definitely not sitting
+                            Instead, I&apos;m trying to do my own thing. I'm not sure what I want to do with my life yet, but it's definitely not sitting
                             at a desk somewhere for 8 hours a day. 
                         </p>
                     
