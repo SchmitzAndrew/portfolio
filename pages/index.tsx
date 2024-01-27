c9a50ce6e1bddcca9da570d86372d2c01d0a48bb
Andrew Schmitz <schmitzandrew03@gmail.com>
2024-01-26 21:50:57 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,10 +31,8 @@ export default function Index({posts}: {posts: PostMeta[]}) {
                 <div className="max-w-4xl mx-auto pt-10 pb-6 px-6">
                     <div className="text-slate-300 font-semibold font-Rubik">
                         <p className="md:text-lg">I&apos;m a software developer creating &nbsp;
-                            <a href="https://usestylize.com" target="_blank" className="underline text-sky-400">usestylize.com</a> &nbsp;
-                            and &nbsp;
-                            <a href="https://usemoment.ai" target="_blank" className="underline text-sky-400">usemoment.ai</a>.
-                            I'm also studying Data Science at UCSD, but honestly, I hate it. I'm terrified of the idea
+                            <a href="https://usestylize.com" target="_blank" className="underline text-sky-400">usestylize.com</a>. &nbsp;
+                            I'm also studying Data Science at UCSD, but honestly, I'm not a huge fan. I'm terrified of the idea
                             of growing up and becoming a spreadsheet monkey or developing algorithms just to get
                             people to click on ads more.
                         </p>
