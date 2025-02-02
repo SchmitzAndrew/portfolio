27430262607dc6b31ba1e713b81d48c1edcebe7a
SchmitzAndrew <schmitzandrew03@gmail.com>
2025-02-02 02:44:36 -0800

    idk abt this whole thing anymore

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import Background from "@/components/Background";
 import { Racing_Sans_One } from "next/font/google";
 
+
 const racingSansOne = Racing_Sans_One({
     weight: '400',
     subsets: ["latin"],
@@ -23,7 +24,7 @@ export default function Page() {
         }
     }, []);
 
-    const cutoutTop = 35 + textHeight;
+    const cutoutTop = -38 + textHeight;
     const cutoutBottom = cutoutTop + 35;
 
     const textStyle = {
@@ -34,15 +35,20 @@ export default function Page() {
     return (
         <Background>
             <div className="w-full h-screen flex items-center justify-center">
-                <div className="h-[60vh] aspect-[1.618] relative">
+                <div className="h-[45vh] aspect-[1.618] relative">
                     <div className="absolute inset-0 bg-black" style={{
                         clipPath: `polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 0%, 30px 0%, 30px ${cutoutBottom}px, calc(30px + ${textWidth}px) ${cutoutBottom}px, calc(30px + ${textWidth}px) ${cutoutTop}px, 30px ${cutoutTop}px, 30px 0%, 0% 0%)`
                     }} />
                     <div ref={containerRef} className="absolute top-8 left-8">
-                        <h1 className={`text-6xl ${racingSansOne.className}`} style={textStyle}>
+                        <h1 className={`text-6xl font-bold italic`} style={textStyle}>
                             andrew schmitz
                         </h1>
+                        <p className="text-white text-lg mt-16 max-w-md">
+                            building <a href="https://myminutes.ai" className="underline">minutes</a> @ <a href="https://slam.ventures">SLAM</a> 
+
+                        </p>
                     </div>
+
                 </div>
             </div>
         </Background>
